8e2c019ee265170049150d360322a82c84f7193d
Joshua Leihe <jleihe@gmail.com>
2015-11-20 09:52:02 -0800

    Working on a basic calculator script to get used to coding in python again :)

---
+++ python/practice/calc/calc.py
@@ -0,0 +1,6 @@
+# File Name: calc.py
+# Written By: Joshua Leihe
+# Purpose: Create a simple python script that takes a string input and translates it to a mathematical equation.  The script should then return the result (or an explanation as to why there was an error).
+
+eq_str = raw_input("Calc 0.1 >> ")
+print "The equation entered was " + eq_str
