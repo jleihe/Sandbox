d56634ddb36f1bd03760a8e9b4751611144d3173
Joshua Leihe <jleihe@gmail.com>
2016-07-12 15:16:07 -0700

    Separated functions into a utils file

--- python/practice/calc/start.py
+++ python/practice/calc/start.py
@@ -2,5 +2,9 @@
 # Written By: Joshua Leihe
 # Purpose: Create a simple python script that takes a string input and translates it to a mathematical equation.  The script should then return the result (or an explanation as to why there was an error).
 
+from utils import *
+
+print "\nWelcome to Calc 0.1!"
+print "Enter \"h\"for help and detailed list of commands!\n\n"
 eq_str = raw_input("Calc 0.1 >> ")
 print "The equation entered was " + eq_str
---
+++ python/practice/calc/utils.py
@@ -0,0 +1,8 @@
+# File Name: calc.py
+# Written By: Joshua Leihe
+# Purpose: Create a simple python script that takes a string input and translates it to a mathematical equation.  The script should then return 
+
+print "\nWelcome to Calc 0.1!"
+print "Enter \"h\"for help and detailed list of commands!\n\n"
+eq_str = raw_input("Calc 0.1 >> ")
+print "The equation entered was " + eq_str
