305eb26366b383f03bb236a72baec21d34d3fdb3
Joshua Leihe <jleihe@gmail.com>
2016-10-29 07:07:34 -0700

    Started the maze project

---
+++ python/practice/algs/maze/utils.py
@@ -0,0 +1,16 @@
+##############################################################################
+#
+# Filename: utils.py
+# Written By: Joshua Leihe
+# Notes : A colection of functions and modules for the maze project
+#
+##############################################################################
+
+# b=black w=white o=occupied
+maze = [['b', 'b', 'b', 'b', 'b'],
+	['b', 'b', 'b', 'b', 'b'],
+	['b', 'b', 'b', 'b', 'b'],
+	['b', 'b', 'b', 'b', 'b'],
+	['b', 'b', 'b', 'b', 'b']]
+
+print maze[2][2]
